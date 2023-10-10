d1523c97bc8494174394fb7f839716dd7a77954e
qtilicous <qtilicousbbz@gmail.com>
2023-10-10 06:56:53 -0700

    still fixing file 5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
+    cees = {'c', 'C'}
 
-    new_string = ''.join(char for char in my_string if char.lower() not in {'c', 'C'})
+    new = ''.join(char for char in my_string if char.lower() not in cees)
 
-    return new_string
+    return new
