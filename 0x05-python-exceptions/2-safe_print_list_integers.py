d84e48a182f14dbc808a2c0a0450cb4c66cc95e7
qtilicous <qtilicousbbz@gmail.com>
2023-10-23 16:08:44 -0700

    Still fixing the file 2-safe_print_list_integers.py

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,23 +1,28 @@
 #!/usr/bin/python3
-def safe_print_list_integers(my_list=[], limit=0):
-    nb_print = 0
-    for i in range(limit):
-        try:
-            print("{:d}".format(my_list[i]), end="")
-            nb_print += 1
-        except (ValueError, TypeError):
-            continue
-        except IndexError:
-            print()
-            break
-    print()
-    return nb_print
+
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    try:
+        for i in range(x):
+            if isinstance(my_list[i], int):
+                print("{:d}".format(my_list[i]), end='')
+                count += 1
+    except (IndexError, ValueError, TypeError):
+        pass
+    finally:
+        print()
+    return count
 
 
-my_list = [12, 7, 'abc', 45, 100, 'def']
-nb_print = safe_print_list_integers(my_list, 2)
-print("nb_print:", nb_print)
-nb_print = safe_print_list_integers(my_list, 5)
-print("nb_print:", nb_print)
-nb_print = safe_print_list_integers(my_list, len(my_list) + 2)
-print("nb_print:", nb_print)
+if __name__ == "__main__":
+    my_list = [1, 2, 3, 4, 5]
+
+    nb_print = safe_print_list_integers(my_list, 2)
+    print("nb_print: {:d}".format(nb_print))
+
+    my_list = [1, 2, 3, "School", 4, 5, [1, 2, 3]]
+    nb_print = safe_print_list_integers(my_list, len(my_list))
+    print("nb_print: {:d}".format(nb_print))
+
+    nb_print = safe_print_list_integers(my_list, len(my_list) + 2)
+    print("nb_print: {:d}".format(nb_print))
