b96f8cb498f902699e744cb178129ab89370c825
qtilicous <qtilicousbbz@gmail.com>
2023-10-24 16:36:18 -0700

    Still fix file 100-singly_linked_list.py

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Module of Square class
+Module of class Singly Linked List.
 """
 
 
@@ -71,3 +71,45 @@ class Node:
         if value is not None and not isinstance(value, Node):
             raise TypeError("next_node must be a Node object")
         self.__next_node = value
+
+
+class SinglyLinkedList:
+    """
+    This class defines a singly linked list.
+    """
+
+    def __init__(self):
+        """
+        Initializes a new singly linked list with an empty head.
+        """
+        self.head = None
+
+    def sorted_insert(self, value):
+        """
+        Inserts a new Node into the correct sorted position in the list.
+
+        Args:
+            value (int): The value to insert into the list.
+        """
+        new_node = Node(value)
+
+        if not self.head or value < self.head.data:
+            new_node.next_node = self.head
+            self.head = new_node
+        else:
+            current = self.head
+            while current.next_node and current.next_node.data < value:
+                current = current.next_node
+            new_node.next_node = current.next_node
+            current.next_node = new_node
+
+    def __str__(self):
+        """
+        Provides a string representation of the singly linked list.
+        """
+        result = []
+        current = self.head
+        while current:
+            result.append(str(current.data))
+            current = current.next_node
+        return '\n'.join(result)
