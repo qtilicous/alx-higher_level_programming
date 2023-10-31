befab619f00ddb5f77655a4702df0be9237607ab
qtilicous <qtilicousbbz@gmail.com>
2023-10-31 15:06:19 -0700

    Still fixing file 19-copy_list.py

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,2 +1,15 @@
+#!/usr/bin/python3
+"""Module of a copy_list class"""
+
+
 def copy_list(original_list):
+    """
+    Copy a list and return a new list.
+
+    Args:
+        original_list (list): The list to be copied.
+
+    Returns:
+        list: A new list that is a copy of the original list.
+    """
     return original_list.copy()
