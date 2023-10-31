8e4b50c4668abd92863f1af98ca38067441dc677
qtilicous <qtilicousbbz@gmail.com>
2023-10-31 04:31:34 -0700

    Add file 0-add_integer.py

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+This module defines a function that adds two integers.
+
+"""
+
+
+def add_integer(a, b=98):
+    """
+    Adds two integers.
+
+    Args:
+        a (int): The first integer.
+        b (int, optional): The second integer. Defaults to 98.
+
+    Returns:
+        int: The sum of a and b.
+
+    Raises:
+        TypeError: If a or b is not an integer or a float that can be cast
+        to an integer.
+    """
+    if not (isinstance(a, int) or isinstance(a, float)):
+        raise TypeError("a must be an integer")
+    if not (isinstance(b, int) or isinstance(b, float)):
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
---
+++ 0x07-python-test_driven_development/README.md
@@ -0,0 +1,2 @@
+This directory contains files with functions for the follwing task:
+0x07-python-test_driven_development
