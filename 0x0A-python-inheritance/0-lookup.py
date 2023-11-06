8ba9b60491dbb28474096b7fbac69a29c876ddcd
qtilicous <qtilicousbbz@gmail.com>
2023-11-06 03:03:41 -0800

    Add file 0-lookup.py

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def lookup(obj):
+    """
+    Returns a list of available attributes and methods of an object.
+
+    Args:
+        obj: The object to inspect.
+
+    Returns:
+        A list of strings representing the attributes and methods.
+    """
+    return dir(obj)
