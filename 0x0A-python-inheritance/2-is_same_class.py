24c8e0e32544fbc42f0199a25aed612f9a288ec1
qtilicous <qtilicousbbz@gmail.com>
2023-11-06 03:28:32 -0800

    Add file 2-is_same_class.py

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def is_same_class(obj, a_class):
+    """
+    Returns True if the object is an instance of the class; else False
+
+    Args:
+        obj: The object to check.
+        a_class: The class to compare against.
+
+    Returns:
+        True if the object is an instance of the specified class; else False.
+    """
+    return type(obj) is a_class
