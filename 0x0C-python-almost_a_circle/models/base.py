b1467e43b77eeb5ebad7d87b14d5f3ede0c5f944
qtilicous <qtilicousbbz@gmail.com>
2023-11-16 14:41:06 -0800

    Fix file base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -8,6 +8,29 @@ class Base:
     """
     The Base class.
     """
+    __nb_objects = 0
+
     def __init__(self, id=None):
         """Initialize the Base instance."""
-        self.id = id if id is not None else id_counter()
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+
+
+if __name__ == "__main__":
+    b1 = Base()
+    print(b1.id)
+
+    b2 = Base()
+    print(b2.id)
+
+    b3 = Base()
+    print(b3.id)
+
+    b4 = Base(12)
+    print(b4.id)
+
+    b5 = Base()
+    print(b5.id)
