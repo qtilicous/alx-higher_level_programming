a75850c7f9781c84a1ca28a09526374fc322d8b1
qtilicous <qtilicousbbz@gmail.com>
2023-11-16 13:40:30 -0800

    Add file file.py

---
+++ 0x0C-python-almost_a_circle/file.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+"""
+my_module.module_file
+
+This module provides a simple example function.
+"""
+
+
+def my_function():
+    """
+    This is a sample function.
+
+    It does nothing but print a message.
+    """
+    print("Hello from my_function!")
+
+
+class MyClass:
+    """
+    A sample class.
+
+    This class doesn't do much, just provides a placeholder.
+    """
+
+    def my_method(self):
+        """
+        A sample method of MyClass.
+
+        This method prints a message.
+        """
+        print("Hello from MyClass.my_method!")
