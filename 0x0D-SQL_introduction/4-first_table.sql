39ae201c5a6dbc3ad3ba514a6a99a4ed25f87b40
qtilicous <qtilicousbbz@gmail.com>
2023-12-12 04:50:02 -0800

    Add file 5-full_table.sql

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -3,8 +3,6 @@
 -- If the table first_table already exists, your script should not fail.
 -- You are not allowed to use the SELECT or SHOW statements.
 
-USE `hbtn_0c_0`;
-
 CREATE TABLE IF NOT EXISTS first_table (
     id INT,
     name VARCHAR(256)
