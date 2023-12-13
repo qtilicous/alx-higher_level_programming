e04c3ffab79692ef6db0a5fc3b813c372ae1908e
qtilicous <qtilicousbbz@gmail.com>
2023-12-13 13:56:08 -0800

    Add file 6-states.sql

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,6 +1,4 @@
 -- Create the table force_name if it doesn't exist
 CREATE TABLE IF NOT EXISTS force_name (
     id INT,
-    name VARCHAR(256) NOT NULL,
-    PRIMARY KEY (id)
-);
+    name VARCHAR(256) NOT NULL);
