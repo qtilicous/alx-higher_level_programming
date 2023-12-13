59ff6c36e30afb5a299e059d89122a30f50fb565
qtilicous <qtilicousbbz@gmail.com>
2023-12-13 14:01:52 -0800

    Add file 7-cities.sql

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -4,8 +4,10 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 -- Use the hbtn_0d_usa database
 USE hbtn_0d_usa;
 
--- Create the table states if it doesn't exist
-CREATE TABLE IF NOT EXISTS states (
+-- Create the table cities if it doesn't exist
+CREATE TABLE IF NOT EXISTS cities (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(256) NOT NULL
+    state_id INT NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    FOREIGN KEY (state_id) REFERENCES states(id)
 );
