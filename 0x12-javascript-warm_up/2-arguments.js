b5ceee58ec6ff9c6609cb55eb6c4fb216b5a2b49
qtilicous <qtilicousbbz@gmail.com>
2024-01-08 11:55:25 -0800

    Fix file 2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
-
-const argsLength = process.argv.length;
-if (argsLength === 2) {
+if (process.argv.length === 2) {
 	console.log('No argument');
-} else if (argsLength === 3) {
+} else if (process.argv.length === 3) {
 	console.log('Argument found');
 } else {
 	console.log('Arguments found');
