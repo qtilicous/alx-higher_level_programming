32178f86eac37e5f71466b6459b90a9027b13082
qtilicous <qtilicousbbz@gmail.com>
2024-01-09 10:03:01 -0800

    Fix file 2-rectangle.js

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -6,8 +6,7 @@ class Rectangle {
       this.width = w;
       this.height = h;
     } else {
-      this.width = undefined;
-      this.height = undefined;
+	    Object.assign(this, {});
     }
   }
 }
