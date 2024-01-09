5c0372e74decb71c0d6f117721dcf147326ad22e
qtilicous <qtilicousbbz@gmail.com>
2024-01-09 09:54:38 -0800

    Add files of task 2-5

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,32 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    } else {
+      this.width = undefined;
+      this.height = undefined;
+    }
+  }
+
+  print () {
+    if (this.width !== undefined && this.height !== undefined) {
+      for (let i = 0; i < this.height; i++) {
+        console.log('X'.repeat(this.width));
+      }
+    }
+  }
+
+  rotate () {
+    [this.width, this.height] = [this.height, this.width];
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+
+module.exports = Rectangle;
