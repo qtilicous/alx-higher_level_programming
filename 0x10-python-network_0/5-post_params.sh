b1967d2af12aa4029a6837b02dd5ee7512c5edca
qtilicous <qtilicousbbz@gmail.com>
2024-01-25 03:08:18 -0800

    Add file to task 3-5

---
+++ 0x10-python-network_0/5-post_params.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# This script takes a URL, sends a POST request with parameters, and displays the body of the response.
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
