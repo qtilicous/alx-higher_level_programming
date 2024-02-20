5427faf99bd3351381f30035b115cbde492c9179
qtilicous <qtilicousbbz@gmail.com>
2024-02-20 10:15:50 -0800

    Try to fix file of task 4 and 5

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,24 +1,12 @@
 #!/usr/bin/node
-
 const request = require('request');
-
-const apiUrl = process.argv[2];
-
-request(apiUrl, function (error, response, body) {
-  if (error) {
-    console.error(error);
-    return;
+request(process.argv[2], function (error, response, body) {
+  if (!error) {
+    const results = JSON.parse(body).results;
+    console.log(results.reduce((count, movie) => {
+      return movie.characters.find((character) => character.endsWith('/18/'))
+        ? count + 1
+        : count;
+    }, 0));
   }
-
-  const films = JSON.parse(body).results;
-  const wedgeAntillesURL = 'https://swapi-api.alx-tools.com/api/people/18/';
-
-  const count = films.reduce((acc, film) => {
-    if (film.characters.includes(wedgeAntillesURL)) {
-      return acc + 1;
-    }
-    return acc;
-  }, 0);
-
-  console.log(count);
 });
